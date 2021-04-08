672a037ea21b9a96a9fa08006a8ff450afaf97fd
Thomas Romuald <thomas.romuald@obs-mip.fr>
2021-04-08 15:08:40 +0200

    i18n

--- src/views/Logging.vue
+++ src/views/Logging.vue
@@ -1,10 +1,20 @@
+<i18n>
+{
+  "en": {
+    "connecting": "Connecting..."
+  },
+  "fr": {
+    "connecting": "Connexion..."
+  }
+}
+</i18n>
 <template>
   <div class="pa-10">
     <div>
       <v-progress-linear indeterminate height="3" />
     </div>
     <div class="text-center pa-10 headline">
-      <p>Connecting...</p>
+      <p>{{ $t('connecting') }}.</p>
     </div>
   </div>
 </template>
